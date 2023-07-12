6ca0118b79e9970262f8178a0f475535c4475769
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-07-12 14:40:17 +0900

    Test Key VM Create

--- main.tf
+++ main.tf
@@ -118,3 +118,25 @@ resource "aws_instance" "public_vm_01" {
     resource = "aws_ec2_instance"
   })))
 }
+
+resource "aws_instance" "public_vm_02" {
+  ami           = data.aws_ami.recent_amazon_linux.id
+  instance_type = var.instnace_type
+  subnet_id     = data.terraform_remote_state.network.outputs.vpc01_public_subnet_01_id
+  associate_public_ip_address = true
+
+  key_name        = "test"
+  vpc_security_group_ids = ["${data.terraform_remote_state.security.outputs.public-vm-sg-id}"]
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    volume_type           = "gp3"
+    volume_size           = 50
+  }
+
+  tags = (merge(local.common-tags, tomap({
+    Name     = "public_vm_02"
+    resource = "aws_ec2_instance"
+  })))
+}
