c0f0ae457b39b3a3e10b165fb918a57fb97b90b6
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-07-12 16:04:16 +0900

    Add Outputs vms public ip

--- output.tf
+++ output.tf
@@ -1,6 +1,14 @@
 output "ami-id" {
   value = data.aws_ami.recent_amazon_linux.id
 }
+
+output "vm1_public_up" {
+  value = aws_instance.public_vm_01.public_ip
+}
+
+output "vm2_public_up" {
+  value = aws_instance.public_vm_02.public_ip
+}
 /* key pair managed by AWS Console
 output "priv_key" {
   value = tls_private_key.ssh-priv-key.private_key_pem
